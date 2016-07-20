c357284abed7aad85a6e01923a5d134586a7f0af
warny_cp <SND\warny_cp>
2016-07-20 07:31:41 +0000

    Add array functions Add IndexedList class

---
+++ AllPurposeUtilities/trunk/Arrays/ArrayUtils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Arrays
+{
+	public static class ArrayUtils
+	{
+		/// <summary>
+		/// Récupère une partie de ce tableau. La sous-parte démarre à une position de caractère spécifiée et a une longueur définie.
+		/// </summary>
+		/// <param name="s">Tableau dont on veut extraire une partie</param>
+		/// <param name="start">Position de caractère de départ de base zéro de la partie à extaire</param>
+		/// <param name="length">Nombre d'éléments à extraires</param>
+		/// <returns>
+		/// Un tableau de longueur length qui commence
+		/// à startIndex dans cette instance, ou System.String.Empty si startIndex est
+		/// égal à la longueur de cette instance et length est égal à zéro.
+		/// </returns>
+		public static T[] Mid<T>( this T[] s, int start, int length )
+		{
+			if (s == null) return null;
+			if (start < 0) start = s.Length + start;
+			if (start <= -length) return new T[] { };
+			if (start < 0) return s.Copy(0, length + start);
+			if (start > s.Length) return new T[] { };
+			if (start + length > s.Length) return s.Copy(start);
+			return s.Copy(start, length);
+		}
+
+		/// <summary>
+		/// Récupère une sous-chaîne de cette instance. La sous-chaîne démarre à une position de caractère spécifiée et a une longueur définie.
+		/// </summary>
+		/// <param name="s">Tableau dont on veut extraire une partie</param>
+		/// <param name="start">Position de caractère de départ de base zéro de la partie à extaire</param>
+		public static T[] Mid<T>( this T[] s, int start )
+		{
+			if (s==null) return null;
+			if (start < 0) start = s.Length + start;
+			if (start < 0) return s;
+			if (start > s.Length) return new T[] { };
+			return s.Copy(start);
+		}
+
+		public static T[] Copy<T>( this T[] array, int start, int length ) {
+			T[] result = new T[length];
+			Array.Copy(array, start, result, 0, length);
+			return result;
+		}
+
+		public static T[] Copy<T>( this T[] array, int start )
+		{
+			return array.Copy(start, array.Length-start);
+		}
+
+	}
+}
---
+++ AllPurposeUtilities/trunk/Lists/IndexedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Lists
+{
+	class IndexedList<K, V> : ICollection<V>, IReadOnlyDictionary<K, V>
+	{
+		private Dictionary<K, V> dictionary;
+		Func<V, K> getKey;
+
+		public IndexedList( Func<V, K> getKey) {
+			this.getKey = getKey;
+		}
+
+		public V this[K key] => dictionary[key];
+		public int Count => dictionary.Count;
+		public bool IsReadOnly => false;
+		public IEnumerable<K> Keys => dictionary.Keys;
+		public IEnumerable<V> Values => dictionary.Values;
+		public void Add( V item ) => dictionary.Add(getKey(item), item);
+		public bool Remove( V item ) => dictionary.Remove(getKey(item));
+		public void Clear() => dictionary.Clear();
+		public bool Contains( V item )=> dictionary.ContainsValue(item);
+		public bool ContainsKey( K key ) => dictionary.ContainsKey(key);
+		public void CopyTo( V[] array, int arrayIndex )	=>dictionary.Values.CopyTo(array, arrayIndex);
+		public IEnumerator<KeyValuePair<K, V>> GetEnumerator() => dictionary.GetEnumerator();
+		public bool TryGetValue( K key, out V value ) => dictionary.TryGetValue(key, out value);
+		IEnumerator<V> IEnumerable<V>.GetEnumerator() => dictionary.Values.GetEnumerator();
+		IEnumerator IEnumerable.GetEnumerator() => dictionary.Values.GetEnumerator();
+	}
+}
