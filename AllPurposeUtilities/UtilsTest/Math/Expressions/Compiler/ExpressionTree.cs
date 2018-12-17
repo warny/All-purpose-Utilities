5f84cd1f441b1b8e5a1f59337105eedab4cb2135
Olivier MARTY <olivier.marty@gmail.com>
2018-12-17 23:25:54 +0100

    Ajout de tests

---
+++ AllPurposeUtilities/UtilsTest/Math/Expressions/Compiler/ExpressionTree.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Mathematics.Expressions.Compiler;
+
+namespace UtilsTest.Math.Expressions.Compiler
+{
+	[TestClass]
+	public class ExpressionTree
+	{
+
+		[TestMethod]
+		public void Declare() {
+			Declare declare = new Declare();
+			declare.TypeName = "string";
+			declare.VariableName = "test";
+			
+		}
+
+		[TestMethod]
+		public void Assignation()
+		{
+			Assignation assignation = new Assignation();
+		}
+	}
+}
