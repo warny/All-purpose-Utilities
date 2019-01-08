97d944e108c2ffeaec1acc212e0b76246df7dc28
Olivier MARTY <olivier.marty+git@gmail.com>
2019-01-08 17:24:32 +0100

    Developpement en cours du résolveur de nom

--- AllPurposeUtilities/trunk/Mathematics/Expressions/Compiler/INameResolver.cs
+++ AllPurposeUtilities/trunk/Mathematics/Expressions/Compiler/INameResolver.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using static System.Reflection.BindingFlags;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Utils.Mathematics.Expressions.Compiler
 {
@@ -16,24 +18,119 @@ namespace Utils.Mathematics.Expressions.Compiler
 		/// <param name="constructorArgumentTypes"></param>
 		/// <returns></returns>
 		Type ResolveType(string typeName, Type[] constructorArgumentTypes);
+
 		/// <summary>
-		/// Récupère la fonction statique du type en paramètre
+		/// Récupère le membre static avec le nom passé en paramètre
+		/// </summary>
+		MemberInfo[] GetStaticMembers(Type t, string memberName);
+
+		/// <summary>
+		/// Récupère le membre d'instance avec le nom passé en paramètre
+		/// </summary>
+		MemberInfo[] GetInstanceMembers(Type t, string memberName);
+
+		/// <summary>
+		/// Récupère la fonction du type en paramètre
 		/// </summary>
 		/// <param name="type">Type dont on veux récupérer la fonction statique</param>
 		/// <param name="functionName">Nom de la fonction</param>
 		/// <param name="argumentsTypes">Type des arguments de la fonction</param>
 		/// <param name="genericArgumentsTypes">Argument génériques explicites</param>
 		/// <returns></returns>
-		MethodInfo ResolveStaticFunction(Type type, string functionName, Type[] argumentsTypes, Type[] genericArgumentsTypes);
-		/// <summary>
-		/// Récupère la fonction d'instance du type en paramètre
-		/// </summary>
-		/// <param name="type">Type dont on veux récupérer la fonction statique</param>
-		/// <param name="functionName">Nom de la fonction</param>
-		/// <param name="argumentsTypes">Type des arguments de la fonction</param>
-		/// <param name="genericArgumentsTypes">Argument génériques explicites</param>
-		/// <returns></returns>
-		MethodInfo ResolveInstanceFunction(Type type, string functionName, Type[] argumentsTypes, Type[] genericArgumentsTypes);
-	
+		MethodInfo ResolveFunction(MemberInfo[] members, Parameter[] parameters, Type[] genericArgumentsTypes);
 	}
+
+	public class Parameter
+	{
+		public int? Position { get; set; }
+		public string Name { get; set; }
+		public Type Type { get; set; }
+	}
+
+	public class DefaultNameResolver : INameResolver
+	{
+		private static Regex selectMember = new Regex(@"(?<name>\w+)`\d+");
+
+		public MemberInfo[] GetInstanceMembers(Type t, string memberName)
+		{
+			IEnumerable<MemberInfo> members = t.GetMembers(Instance | Public | GetField | GetProperty | InvokeMethod);
+			members = members.Where(m => selectMember.Match(m.Name).Groups["name"].Value == memberName);
+			return members.ToArray();
+		}
+
+		public MemberInfo[] GetStaticMembers(Type t, string memberName)
+		{
+			IEnumerable<MemberInfo> members = t.GetMembers(Static | Public | GetField | GetProperty | InvokeMethod);
+			members = members.Where(m => selectMember.Match(m.Name).Groups["name"].Value == memberName);
+			return members.ToArray();
+		}
+
+		public MethodInfo ResolveFunction(MemberInfo[] members, Parameter[] parameters, Type[] genericArgumentsTypes)
+		{
+			int distance = int.MaxValue;
+			MethodInfo methodInfo = null;
+			foreach (var member in members) {
+				if (member is MethodInfo mi) {
+					var computedDistance = Distance(mi, parameters, genericArgumentsTypes);
+					if (computedDistance < distance) methodInfo = mi;
+				}
+			}
+
+			return methodInfo;
+		}
+
+		private int? Distance(MethodInfo methodInfo, Parameter[] parameters, Type[] genericArgumentsTypes)
+		{
+			if (methodInfo.IsGenericMethod && genericArgumentsTypes == null) {
+				//resoudre automatiquement les arguments génériques
+			}
+			else if (methodInfo.IsGenericMethod && genericArgumentsTypes != null) {
+				if (methodInfo.GetGenericArguments().Length == genericArgumentsTypes.Length) {
+					methodInfo = methodInfo.MakeGenericMethod(genericArgumentsTypes);
+				}
+				else {
+					return null;
+				}
+			}
+
+			var methodParameters = methodInfo.GetParameters();
+
+			int distance = 0;
+			foreach (var parameter in parameters) {
+				ParameterInfo methodParameter;
+				if (parameter.Position != null) {
+					methodParameter = methodParameters?[parameter.Position.Value];
+					if (methodParameter == null) {
+						methodParameter = methodParameters.LastOrDefault();
+						if (methodParameter == null) return null;
+						if (methodParameter.GetCustomAttribute<ParamArrayAttribute>() == null) return null;
+					}
+				}
+				else if (parameter.Name != null) {
+					methodParameter = methodParameters.FirstOrDefault(p => p.Name == parameter.Name);
+					if (methodParameters == null) return null;
+				}
+				else {
+					return null;
+				}
+
+				Type testType = parameter.Type;
+				var objectType = typeof(object);
+				while (parameter.Type != methodParameter.ParameterType) {
+					if (testType == typeof(object)) return null;
+					distance++;
+					testType = testType.BaseType;
+				}
+			}
+			return distance;
+			
+		}
+
+		public Type ResolveType(string typeName, Type[] constructorArgumentTypes)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+
 }
