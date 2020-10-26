3e39c1bf5d3ae125027439131823d3c993b45287
Olivier MARTY <olivier.marty+git@gmail.com>
2020-10-26 18:19:09 +0100

    Suppression des copies de Stream (CopyTo est plus effficace) Ajout d'une fonction de lecture de tous les octets du stream

--- Utils/Streams/StreamUtils.cs
+++ Utils/Streams/StreamUtils.cs
@@ -23,39 +23,15 @@ namespace Utils.Streams
 			return result;
 		}
 
-		/// <summary>
-		/// Copies data from a source stream to a target stream.</summary>
-		/// <param name="source">The source stream to copy from.</param>
-		/// <param name="target">The destination stream to copy to.</param>
-		/// <param name="length">Data Length to copy</param>
-		public static void Copy( IO.Stream source, IO.Stream target, int length )
+		public static byte[] ReadToEnd(this Stream s)
 		{
-			const int bufferLength = 8196;
-			byte[] buffer = new byte[bufferLength];
-			while (length > bufferLength) {
-				source.Read(buffer, 0, bufferLength);
-				target.Write(buffer, 0, bufferLength);
-
-				length-=bufferLength;
+			List<byte> result = new List<byte>(512);
+			byte[] temp = new byte[512];
+			for (var dataLength = s.Read(temp, 0, temp.Length); dataLength == 512; dataLength = s.Read(temp, 0, temp.Length))
+			{
+				result.AddRange(temp);
 			}
-			source.Read(buffer, 0, length);
-			target.Write(buffer, 0, length);
+			return result.ToArray();
 		}
-
-
-		/// <summary>
-		/// Copies data from a source stream to a target stream.</summary>
-		/// <param name="source">The source stream to copy from.</param>
-		/// <param name="target">The destination stream to copy to.</param>
-		public static void Copy( Stream source, Stream target )
-		{
-			const int bufferLength = 8196;
-			byte[] buf = new byte[bufferLength];
-			int bytesRead = 0;
-			while ((bytesRead = source.Read(buf, 0, bufferLength)) > 0) {
-				target.Write(buf, 0, bytesRead);
-			}
-		}
-
 	}
 }
