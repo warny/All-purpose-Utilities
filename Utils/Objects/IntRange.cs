2cf06f45eb4b730e1b20949b875f1d6edc504cd6
Olivier MARTY <olivier.marty+git@gmail.com>
2020-11-20 11:17:49 +0100

    Ajout de la classe intRange

---
+++ Utils/Objects/IntRange.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+using Utils.Mathematics;
+using System.Collections;
+
+namespace Utils.Objects
+{
+	public class IntRange : IEnumerable<int>
+	{
+		private class SimpleRange : IComparable<SimpleRange>, IComparable<int>
+		{
+			public SimpleRange(int minimum, int maximum)
+			{
+				Minimum = minimum;
+				Maximum = maximum;
+			}
+
+			public int Minimum { get; }
+			public int Maximum { get; }
+
+			public int CompareTo(SimpleRange other)
+			{
+				if (other == null) return -1;
+				return new[] {
+					Minimum.CompareTo(other.Minimum),
+					Maximum.CompareTo(other.Maximum)
+				}.FirstOrDefault(n => n != 0);
+			}
+
+			public int CompareTo(int other)
+			{
+				if (other < Minimum) return 1;
+				if (other > Maximum) return -1;
+				return 0;
+			}
+
+			public static bool CanMerge(SimpleRange range1, SimpleRange range2) => range1.Minimum <= range2.Maximum + 1 && range2.Minimum <= range1.Maximum + 1;
+			public static SimpleRange Merge(SimpleRange range1, SimpleRange range2)
+			{
+				if (range1 == null) return range2;
+				if (range2 == null) return range1;
+				if (!CanMerge(range1, range2)) return null;
+				return new SimpleRange(MathEx.Min(range1.Minimum, range2.Minimum), MathEx.Max(range1.Maximum, range2.Maximum));
+			}
+
+			public static SimpleRange[] Except(SimpleRange range1, SimpleRange range2)
+			{
+				if (range2 == null || range2.Minimum > range1.Maximum || range2.Maximum < range1.Minimum) return new SimpleRange[] { new SimpleRange(range1.Minimum, range1.Maximum) };
+				if (range2.Minimum <= range1.Minimum && range2.Maximum >= range1.Maximum) return new SimpleRange[] { };
+				if (range2.Maximum >= range1.Maximum) return new SimpleRange[] { new SimpleRange(range1.Minimum, range2.Minimum - 1) };
+				if (range2.Minimum <= range1.Minimum) return new SimpleRange[] { new SimpleRange(range2.Maximum + 1, range1.Maximum) };
+				return new SimpleRange[] {
+					new SimpleRange(range1.Minimum, range2.Minimum - 1),
+					new SimpleRange(range2.Maximum + 1, range1.Maximum)
+				};
+			}
+
+			public override string ToString() => Minimum == Maximum ? Minimum.ToString() : $"{Minimum}-{Maximum}";
+		}
+
+		private readonly SortedSet<SimpleRange> ranges = new SortedSet<SimpleRange>();
+
+		private IntRange() { }
+
+		public IntRange(string range)
+		{
+			SortedSet<SimpleRange> result = new SortedSet<SimpleRange>();
+			var matches = Regex.Matches(range, @"((?<singleValue>\d+)|(?<start>\d+)-(?<end>\d+))(,|;|$)", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+			foreach (Match match in matches)
+			{
+				if (match.Groups["singleValue"].Success)
+				{
+					var value = int.Parse(match.Groups["singleValue"].Value);
+					result.Add(new SimpleRange(value, value));
+				}
+				else
+				{
+					var start = int.Parse(match.Groups["start"].Value);
+					var end = int.Parse(match.Groups["end"].Value);
+					result.Add(new SimpleRange(start, end));
+				}
+			}
+			InitRanges(result);
+		}
+
+		private void InitRanges(IEnumerable<SimpleRange> result)
+		{
+			SimpleRange current = null;
+			foreach (var simpleRange in result)
+			{
+				var newRange = SimpleRange.Merge(current, simpleRange);
+				if (newRange == null)
+				{
+					ranges.Add(current);
+					current = simpleRange;
+				}
+				else
+				{
+					current = newRange;
+				}
+			}
+			if (current!=null) ranges.Add(current);
+		}
+
+		public bool Contains(int value)
+		{
+			foreach (var range in ranges)
+			{
+				var comparison = range.CompareTo(value);
+				if (comparison == 0) return true;
+				if (comparison == 1) return false;
+			}
+			return false;
+		}
+
+		public IEnumerator<int> GetEnumerator()
+		{
+			IEnumerable<int> getValues(IEnumerable<SimpleRange> ranges)
+			{
+				foreach (var range in ranges)
+				{
+					for (int i = range.Minimum; i <= range.Maximum; i++)
+					{
+						yield return i;
+					}
+				}
+			}
+
+			return getValues(this.ranges).GetEnumerator();
+
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+		public override string ToString() => string.Join(";", ranges.Select(r => r.ToString()));
+
+		public static IntRange operator +(IntRange range1, IntRange range2)
+		{
+			var result = new IntRange();
+			var list = new List<SimpleRange>();
+			var enum1 = range1.ranges.GetEnumerator();
+			var enum2 = range2.ranges.GetEnumerator();
+			enum1.MoveNext();
+			enum2.MoveNext();
+			while (true)
+			{
+				var c = enum1.Current?.CompareTo(enum2.Current) ?? 1;
+				if (c < 0)
+				{
+					list.Add(enum1.Current);
+					if (!enum1.MoveNext())
+					{
+						list.Add(enum2.Current);
+						while (enum2.MoveNext())
+						{
+							list.Add(enum2.Current);
+						}
+						break;
+					}
+				}
+				else
+				{
+					list.Add(enum2.Current);
+					if (!enum2.MoveNext())
+					{
+						list.Add(enum1.Current);
+						while (enum1.MoveNext())
+						{
+							list.Add(enum1.Current);
+						}
+						break;
+					}
+				}
+			}
+			result.InitRanges(list);
+			return result;
+		}
+
+		public static IntRange operator-(IntRange range1, IntRange range2)
+		{
+			var ranges = new List<SimpleRange>();
+
+			var enum2 = range2.ranges.GetEnumerator();
+			enum2.MoveNext();
+			bool find = true;
+			foreach (var simpleRange in range1.ranges)
+			{
+				if (find)
+				{
+					var current = simpleRange;
+					while (current != null)
+					{
+						while ((enum2.Current?.Maximum ?? int.MaxValue) < current.Minimum)
+						{
+							find = enum2.MoveNext();
+							if (!find) break;
+						}
+						var excepted = SimpleRange.Except(current, enum2.Current);
+						if (excepted.Length == 0) break;
+						ranges.Add(excepted[0]);
+						if (excepted.Length == 1) break;
+						current = excepted[1];
+					}
+				}
+				else
+				{
+					ranges.Add(new SimpleRange(simpleRange.Minimum, simpleRange.Maximum));
+				}
+			}
+			var result = new IntRange();
+			result.InitRanges(ranges);
+			return result;
+		}
+	}
+}
---
+++ UtilsTest/Objects/IntRangeTest.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utils.Objects;
+
+namespace UtilsTest.Objects
+{
+	[TestClass]
+	public class IntRangeTest
+	{
+		[TestMethod]
+		public void IntRangeTest1()
+		{
+			var tests = new (string test, string result)[] {
+				("1;2;3", "1-3"),
+				("3;2;1", "1-3"),
+				("1;3", "1;3"),
+				("1-3", "1-3"),
+				("1-3;5", "1-3;5"),
+				("5;1-3", "1-3;5"),
+				("1-3;4", "1-4"),
+				("1-3;4-6", "1-6"),
+				("4-6;1-3;", "1-6"),
+				("1-4;3-6", "1-6"),
+				("3;1-4;3-6", "1-6"),
+			};
+
+			foreach (var test in tests)
+			{
+				var intRange = new IntRange(test.test);
+				Assert.AreEqual(test.result, intRange.ToString());
+			}
+		}
+
+		[TestMethod]
+		public void IntRangeTest2()
+		{
+			var tests = new (string test1, string test2, string result)[] {
+				("1", "3", "1;3"),
+				("1", "2", "1-2"),
+				("1-2", "3", "1-3"),
+				("1", "2-3", "1-3"),
+				("1-2", "4-5", "1-2;4-5"),
+				("1-2;7-9", "4-5", "1-2;4-5;7-9"),
+				("1-3", "4-5", "1-5"),
+				("1-3;7-9", "4-6", "1-9"),
+				("1-3;5-9", "4", "1-9"),
+				("1-3;5-9", "2-6", "1-9"),
+			};
+
+			void doTest (string test1, string test2, string expected)
+			{
+				var range1 = new IntRange(test1);
+				var range2 = new IntRange(test2);
+				var result = range1 + range2;
+				Assert.AreEqual(expected, result.ToString());
+			}
+
+			foreach (var test in tests)
+			{
+				doTest(test.test1, test.test2, test.result);
+				doTest(test.test2, test.test1, test.result);
+			}
+		}
+
+		[TestMethod]
+		public void IntRangeTest3()
+		{
+			var tests = new (string test1, string test2, string result)[] {
+				("1", "3", "1"),
+				("1", "2", "1"),
+				("1", "1", ""),
+				("1-2", "3", "1-2"),
+				("1-2", "3-4", "1-2"),
+				("1-5", "3", "1-2;4-5"),
+				("1-5", "3-5", "1-2"),
+				("1-5", "2;4", "1;3;5"),
+				("1-5;7-9", "2;4;8", "1;3;5;7;9"),
+				("1-5;7-9", "4-7", "1-3;8-9"),
+				("1-5;7-9", "4-9", "1-3"),
+				("1-5;7-9", "1-7", "8-9"),
+			};
+
+			foreach (var test in tests)
+			{
+				var range1 = new IntRange(test.test1);
+				var range2 = new IntRange(test.test2);
+				var result = range1 - range2;
+				Assert.AreEqual(test.result, result.ToString());
+			}
+		}
+
+		[TestMethod]
+		public void IntRangeTest4()
+		{
+			var tests = new (string range, int[] values, bool result)[] {
+				("1", new [] { 1 }, true),
+				("1", new [] { 0, 2 }, false),
+				("1-3", new [] { 1, 2, 3 }, true),
+				("1-3", new [] { 0, 4 }, false),
+				("1-3; 5-7", new [] { 1, 2, 3, 5, 6, 7 }, true),
+				("1-3; 5-7", new [] { 0, 4, 8 }, false),
+				("1; 5-7", new [] { 1, 5, 6, 7 }, true),
+				("1; 5-7", new [] { 0, 2,3, 4, 8 }, false),
+			};
+
+			foreach (var test in tests)
+			{
+				var range = new IntRange(test.range);
+				foreach (var value in test.values)
+				{
+					Assert.AreEqual(test.result, range.Contains(value), $"{value} {(test.result ? "n'a pas": "a")} été trouvé dans {range}");
+				}
+			}
+		}
+
+		[TestMethod]
+		public void IntRangeTest5()
+		{
+			var comparer = new Utils.Arrays.ArrayEqualityComparer<int>();
+
+			var tests = new (string range, int[] result)[] {
+				("1", new [] { 1 }),
+				("1-3", new [] { 1, 2, 3 }),
+				("1-3; 5-7", new [] { 1, 2, 3, 5, 6, 7 }),
+				("1; 5-7", new [] { 1, 5, 6, 7 }),
+			};
+
+			foreach (var test in tests)
+			{
+				var range = new IntRange(test.range);
+				var result = range.ToArray();
+				Assert.IsTrue(comparer.Equals(test.result, result), $"{{{string.Join(",", result)}}} est différent de {{{string.Join(",", test.result)}}}");
+			}
+		}
+	}
+}
