ce398bcf8947d7ca2687a42d2a80b3d1f4877068
Olivier MARTY <olivier.marty@onesys.fr>
2022-01-25 11:22:42 +0100

    Correction yaml

--- UtilsTest/Files/PathUtilsTest.cs
+++ UtilsTest/Files/PathUtilsTest.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Utils.Files;
 
 namespace UtilsTest.Files
 {
 	[TestClass]
 	public class PathUtilsTest
 	{
-		//[TestMethod]
-		//public void EnumerateFiles()
-		//{
-		//	var files = PathUtils.EnumerateFiles(@"c:\program files\*\*\*\*.exe").ToArray();
-		//	Assert.IsTrue(files.Any());
-		//}
+		[TestMethod]
+		[Ignore]
+		public void EnumerateFiles()
+		{
+			var files = PathUtils.EnumerateFiles(@"c:\program files\*\*\*\*.exe").ToArray();
+			Assert.IsTrue(files.Any());
+		}
 
 	}
 }
