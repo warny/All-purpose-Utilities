fc79255be7298d32ced544b8cc6a628aa36a1bcf
Olivier MARTY <olivier.marty@gmail.com>
2023-03-02 14:45:47 +0100

    Ajout d'un test de dérivation

--- UtilsTest/Mathematics/Expressions/ExpressionDerivationTests.cs
+++ UtilsTest/Mathematics/Expressions/ExpressionDerivationTests.cs
@@ -12,70 +12,6 @@ namespace UtilsTest.Mathematics.Expressions
 		ExpressionDerivation derivation = new ExpressionDerivation("x");
 
 		[TestMethod]
-		public void SimpleExpressionTest1()
-		{
-			Expression<Func<double, double>> expression = x => x;
-			Expression<Func<double, double>> derivateTarget = x => 1;
-			var derivate = derivation.Derivate(expression);
-			Assert.AreEqual(derivateTarget, derivate, ExpressionComparer.Default);
-		}
-
-
-		[TestMethod]
-		public void SimpleExpressionTest2()
-		{
-			Expression<Func<double, double>> expression = x => x*x;
-			Expression<Func<double, double>> derivateTarget = x => 2 * x;
-			var derivate = derivation.Derivate(expression);
-			Assert.AreEqual(derivateTarget, derivate, ExpressionComparer.Default);
-		}
-
-		[TestMethod]
-		public void SimpleExpressionTest3()
-		{
-			Expression<Func<double, double>> expression = x => x*x*x;
-			Expression<Func<double, double>> derivateTarget = y => 3 * y * y;
-			var derivate = derivation.Derivate(expression);
-			Assert.AreEqual(derivateTarget, derivate, ExpressionComparer.Default);
-		}
-
-		[TestMethod]
-		public void SimpleExpressionTest4()
-		{
-			Expression<Func<double, double>> expression = x => System.Math.Cos(x);
-			Expression<Func<double, double>> derivateTarget = x => -System.Math.Sin(x);
-			var derivate = derivation.Derivate(expression);
-			Assert.AreEqual(derivateTarget, derivate, ExpressionComparer.Default);
-		}
-
-		[TestMethod]
-		public void SimpleExpressionTest5()
-		{
-			Expression<Func<double, double>> expression = x => System.Math.Sin(x);
-			Expression<Func<double, double>> derivateTarget = x => System.Math.Cos(x);
-			var derivate = derivation.Derivate(expression);
-			Assert.AreEqual(derivateTarget, derivate, ExpressionComparer.Default);
-		}
-
-		[TestMethod]
-		public void SimpleExpressionTest6()
-		{
-			Expression<Func<double, double>> expression = x => System.Math.Exp(x);
-			Expression<Func<double, double>> derivateTarget = x => System.Math.Exp(x);
-			var derivate = derivation.Derivate(expression);
-			Assert.AreEqual(derivateTarget, derivate, ExpressionComparer.Default);
-		}
-
-		[TestMethod]
-		public void SimpleExpressionTest7()
-		{
-			Expression<Func<double, double>> expression = x => System.Math.Exp(x*x);
-			Expression<Func<double, double>> derivateTarget = x => 2 * x * System.Math.Exp(x*x);
-			var derivate = derivation.Derivate(expression);
-			Assert.AreEqual(derivateTarget, derivate, ExpressionComparer.Default);
-		}
-
-		[TestMethod]
 		public void ExpressionsTests()
 		{
 			var parameters = new ParameterExpression[] {
@@ -92,6 +28,7 @@ namespace UtilsTest.Mathematics.Expressions
 				("x^3 + x^2 + x+1 ", "3*x^2 + 2*x + 1"),
 				("Cos(x)", "0-Sin(x)"),
 				("Sin(x)", "Cos(x)"),
+				("Sin(2*x)", "2*Cos(2*x)"),
 				("(Sin(x)) * (Cos(x))", "(Cos(x))^2-(Sin(x))^2"),
 				("Exp(x^2)", "2*x*Exp(x^2)"),
 			};
