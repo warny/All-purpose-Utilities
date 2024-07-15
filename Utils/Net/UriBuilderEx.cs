bfbea988dc06114eee7494a1ec6e02db7772607f
Olivier MARTY <olivier_marty@outlook.com>
2024-07-15 11:42:27 +0200

    Modification de UriBuilder en UriBuilderEx Gestion de la génération de la chaîne d'authentification basique

--- Utils/Net/UriBuilderEx.cs
+++ Utils/Net/UriBuilderEx.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Text;
 using Utils.Objects;
 
 namespace Utils.Net
 {
-	public class UriBuilder
+	public class UriBuilderEx
 	{
 		public static readonly IReadOnlyDictionary<string, int> DefaultPorts = new ReadOnlyDictionary<string, int>(new Dictionary<string, int>{
 			{Uri.UriSchemeFtp, 21},
@@ -25,7 +26,7 @@ namespace Utils.Net
 			{ "ftps", 990 }
 		});
 
-		public UriBuilder(Uri uri)
+		public UriBuilderEx(Uri uri)
 		{
 			uri.ArgMustNotBeNull();
 
@@ -47,7 +48,7 @@ namespace Utils.Net
 			this.Fragment = uri.Fragment?.StartsWith("#") ?? false ? uri.Fragment.Substring(1) : uri.Fragment;
 		}
 
-		public UriBuilder(string uriString) : this(new Uri(uriString)) { }
+		public UriBuilderEx(string uriString) : this(new Uri(uriString)) { }
 
 
 		public string Scheme { get; set; }
@@ -59,7 +60,15 @@ namespace Utils.Net
 		public string Fragment { get; set; }
 		public NameValueCollection QueryString { get; set; }
 
-		public override string ToString()
+		public string GetBasicAuthorizationBase64String()
+		{
+			if (string.IsNullOrWhiteSpace(Username) && string.IsNullOrWhiteSpace(Password)) return "";
+			var urlEncodedUsername = System.Web.HttpUtility.UrlEncode(Username);
+			var urlEncodedPassword = System.Web.HttpUtility.UrlEncode(this.Password);
+			return Convert.ToBase64String(Encoding.ASCII.GetBytes($"{urlEncodedUsername}:{urlEncodedPassword}"));
+		}
+
+		private UriBuilder InnerBuildUrl()
 		{
 			int port;
 			if (DefaultPorts.TryGetValue(this.Scheme, out int defaultPort) && this.Port == defaultPort)
@@ -71,17 +80,43 @@ namespace Utils.Net
 				port = this.Port;
 			}
 
-			System.UriBuilder builder = new System.UriBuilder(this.Scheme, this.Host, port, this.AbsolutePath);
-			builder.UserName = Username;
-			builder.Password = Password;
+			UriBuilder builder = new UriBuilder(this.Scheme, this.Host, port, this.AbsolutePath);
 			var queryElements = QueryString
 				.Cast<string>()
 				.SelectMany(key => QueryString.GetValues(key).Select(value => $"{System.Web.HttpUtility.UrlEncode(key)}={System.Web.HttpUtility.UrlEncode(value)}"));
 
 			builder.Query = string.Join("&", queryElements);
-			builder.Fragment = Fragment.IsNullOrWhiteSpace() ? "" : "#" + Fragment;
+			builder.Fragment = string.IsNullOrWhiteSpace(Fragment) ? "" : "#" + Fragment;
+			return builder;
+		}
 
+		public string GetFullUrl()
+		{
+			UriBuilder builder = InnerBuildUrl();
+
+			builder.UserName = Username;
+			builder.Password = Password;
 			return builder.ToString();
+		}
+
+		public string GetUrlWithoutAuthorization()
+		{
+			UriBuilder builder = InnerBuildUrl();
+			return builder.ToString();
+		}
+
+		public override string ToString() => ToString("", null);
+		public string ToString(string format) => ToString(format, null);
+
+		public string ToString(string? format, IFormatProvider? formatProvider)
+		{
+			switch (format)
+			{
+				case "s":
+					return GetUrlWithoutAuthorization();
+				default:
+					return GetFullUrl();
+			}
 		}
 	}
 
