f7cb8b20fe74cbcde3d6b70e0496c5b010c66f11
warny <olivier.marty@gmail.com>
2025-06-21 18:27:40 +0200

    Store offsets in immutable array

--- Utils/Arrays/ArrayAccessor.cs
+++ Utils/Arrays/ArrayAccessor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using Utils.Objects;
@@ -109,7 +110,18 @@ public class ArrayAccessor<T> : ArrayAccessor<T, T[]>
 	/// <summary>
 	/// Gets the offset used in the underlying array to access elements.
 	/// </summary>
-	public int Offset { get; }
+        public int Offset { get; }
+
+        /// <summary>
+        /// Pre-computed offsets for each dimension except the last one.
+        /// Each inner array contains the offsets for a given dimension.
+        /// </summary>
+        private readonly ImmutableArray<ImmutableArray<int>> offsetTables;
+
+        /// <summary>
+        /// Multipliers used to compute offsets when creating spans.
+        /// </summary>
+        private readonly int[] multipliers;
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="ArrayAccessor{T}"/> class.
@@ -117,10 +129,27 @@ public class ArrayAccessor<T> : ArrayAccessor<T, T[]>
 	/// <param name="array">The underlying one-dimensional array.</param>
 	/// <param name="offset">The offset in the array where elements start.</param>
 	/// <param name="dimensions">The sizes of each dimension of the array.</param>
-	public ArrayAccessor(T[] array, int offset, params int[] dimensions) : base(array, dimensions)
-	{
-		this.Offset = offset.ArgMustBeGreaterOrEqualsThan(0);
-	}
+        public ArrayAccessor(T[] array, int offset, params int[] dimensions) : base(array, dimensions)
+        {
+                this.Offset = offset.ArgMustBeGreaterOrEqualsThan(0);
+
+                var builder = ImmutableArray.CreateBuilder<ImmutableArray<int>>(Dimensions - 1);
+                multipliers = new int[Dimensions];
+
+                int multiplier = 1;
+                for (int i = Dimensions - 1; i >= 0; i--)
+                {
+                        multipliers[i] = multiplier;
+                        if (i < Dimensions - 1)
+                        {
+                                int[] offsets = new int[Sizes[i]];
+                                for (int j = 0; j < Sizes[i]; j++) offsets[j] = j * multiplier;
+                                builder.Insert(0, ImmutableArray.Create(offsets));
+                        }
+                        multiplier *= Sizes[i];
+                }
+                offsetTables = builder.ToImmutable();
+        }
 
 	/// <summary>
 	/// Validates that the underlying array has sufficient size for the specified dimensions.
@@ -139,15 +168,16 @@ public class ArrayAccessor<T> : ArrayAccessor<T, T[]>
 	/// <param name="references">The indices in each dimension.</param>
 	/// <returns>The position in the one-dimensional array.</returns>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	private int Position(int[] references)
-	{
-		int position = 0;
-		for (int i = 0; i < references.Length; i++)
-		{
-			position = position * Sizes[i] + references[i];
-		}
-		return position + Offset;
-	}
+        private int Position(int[] references)
+        {
+                int position = Offset;
+                for (int i = 0; i < references.Length - 1; i++)
+                {
+                        position += offsetTables[i][references[i]];
+                }
+                position += references[^1];
+                return position;
+        }
 
 	/// <summary>
 	/// Retrieves the element at the specified multi-dimensional index.
@@ -162,6 +192,46 @@ public class ArrayAccessor<T> : ArrayAccessor<T, T[]>
 	/// </summary>
 	/// <param name="value">The value to set.</param>
 	/// <param name="references">The indices in each dimension.</param>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	protected override void SetElement(T value, int[] references) => this.innerObject[Position(references)] = value;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void SetElement(T value, int[] references) => this.innerObject[Position(references)] = value;
+
+        /// <summary>
+        /// Retrieves a <see cref="Span{T}"/> covering the requested indexes.
+        /// </summary>
+        /// <param name="indexes">
+        /// Indexes for each dimension. If fewer indexes than dimensions are
+        /// provided, the returned span will include all elements of the
+        /// remaining sub-dimensions.
+        /// </param>
+        /// <returns>A span over the specified section of the underlying array.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<T> AsSpan(params IEnumerable<int>[] indexes)
+        {
+                List<int> list = new();
+                foreach (IEnumerable<int> e in indexes) list.AddRange(e);
+                int[] refs = list.ToArray();
+                if (refs.Length > Dimensions)
+                        throw new ArgumentException("Reference dimensions do not match array dimensions.");
+
+                for (int i = 0; i < refs.Length; i++)
+                {
+                        if (refs[i] < 0 || refs[i] >= Sizes[i])
+                                throw new IndexOutOfRangeException($"Index {refs[i]} is out of bounds for dimension {i}.");
+                }
+
+                int position = Offset;
+                for (int i = 0; i < refs.Length; i++)
+                {
+                        if (i < offsetTables.Length)
+                                position += offsetTables[i][refs[i]];
+                        else
+                                position += refs[i];
+                }
+
+                int length = 1;
+                for (int i = refs.Length; i < Dimensions; i++)
+                        length *= Sizes[i];
+
+                return this.innerObject.AsSpan(position, length);
+        }
 }
