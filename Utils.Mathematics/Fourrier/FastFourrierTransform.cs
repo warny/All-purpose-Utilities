7ee88b04402ae4252ebbfbaa3f07e932b635e2fc
Olivier MARTY <olivier_marty@outlook.com>
2025-06-23 17:06:42 +0200

    Optimisations de la transformée de fourrier

--- Utils.Mathematics/Fourrier/FastFourrierTransform.cs
+++ Utils.Mathematics/Fourrier/FastFourrierTransform.cs
@@ -19,13 +19,14 @@ public class FastFourrierTransform
 	private void Separate(Complex[] array, int start, int end)
 	{
 		int n = end - start;
-		Complex[] buffer = new Complex[n / 2];  // get temp heap storage
-		for (int i = 0; i < n / 2; i++)    // copy all odd elements to heap storage
-			buffer[i] = array[i * 2 + 1];
-		for (int i = 0; i < n / 2; i++)    // copy all even elements to lower-half of a[]
-			array[i] = array[i * 2];
-		for (int i = 0; i < n / 2; i++)    // copy all odd (from heap) to upper-half of a[]
-			array[i + n / 2] = buffer[i];
+		var n2 = n >> 1;
+		Complex[] buffer = new Complex[n2];  // get temp heap storage
+		for (int i = 0; i < n2; i++)    // copy all odd elements to heap storage
+			buffer[i] = array[(i << 1) | 1];
+		for (int i = 0; i < n2; i++)    // copy all even elements to lower-half of a[]
+			array[i] = array[i << 1];
+		for (int i = 0; i < n2; i++)    // copy all odd (from heap) to upper-half of a[]
+			array[i + n2] = buffer[i];
 	}
 
 	/// <summary>
@@ -52,24 +53,25 @@ public class FastFourrierTransform
 	/// <param name="end">Exclusive end index.</param>
 	public void Transform(Complex[] array, int start, int end)
 	{
-		int N = end - start;
-		if (N < 2)
+		int n = end - start;
+		if (n < 2)
 		{
 			return;
 		}
+		int n2 = n >> 1;
 
 		Separate(array, start, end);      // all evens to lower half, all odds to upper half
-		Transform(array, start, start + N / 2);   // recurse even items
-		Transform(array, start + N / 2, end);   // recurse odd  items
+		Transform(array, start, start + n2);   // recurse even items
+		Transform(array, start + n2, end);   // recurse odd  items
 												// combine results of two half recursions
-		for (int k = 0; k < N / 2; k++)
+		for (int k = 0; k < n2; k++)
 		{
 			Complex e = array[k];   // even
-			Complex o = array[k + N / 2];   // odd
+			Complex o = array[k + n2];   // odd
 											// w is the "twiddle-factor"
-			Complex w = Complex.Exp(new Complex(0, -2 * Math.PI * k / N));
+			Complex w = Complex.Exp(new Complex(0, -2 * Math.PI * k / n));
 			array[k] = e + w * o;
-			array[k + N / 2] = e - w * o;
+			array[k + n2] = e - w * o;
 		}
 	}
 }
