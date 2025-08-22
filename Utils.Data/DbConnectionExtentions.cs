9cffd9380d72b1ed12f9702c8ba8073418e8e1a1
Olivier MARTY <olivier_marty@outlook.com>
2025-08-22 12:16:59 +0200

    Indentation corrections

--- Utils.Data/DbConnectionExtentions.cs
+++ Utils.Data/DbConnectionExtentions.cs
@@ -8,15 +8,15 @@ namespace Utils.Data;
 /// </summary>
 public static class DbConnectionExtensions
 {
-        /// <summary>
-        /// Creates a command using an interpolated SQL builder.
-        /// </summary>
-        /// <param name="dbConnection">The connection used to create the command.</param>
-        /// <param name="interpolator">Interpolator that builds the SQL and parameters.</param>
-        /// <returns>A command initialized with text and parameters provided by <paramref name="interpolator"/>.</returns>
-        public static IDbCommand CreateCommand(this IDbConnection dbConnection, [InterpolatedStringHandlerArgument(nameof(dbConnection))] SqlBuilderInterpolator interpolator)
-        {
-                interpolator.DbCommand.CommandText = interpolator.ToString();
-                return interpolator.DbCommand;
-        }
+	/// <summary>
+	/// Creates a command using an interpolated SQL builder.
+	/// </summary>
+	/// <param name="dbConnection">The connection used to create the command.</param>
+	/// <param name="interpolator">Interpolator that builds the SQL and parameters.</param>
+	/// <returns>A command initialized with text and parameters provided by <paramref name="interpolator"/>.</returns>
+	public static IDbCommand CreateCommand(this IDbConnection dbConnection, [InterpolatedStringHandlerArgument(nameof(dbConnection))] SqlBuilderInterpolator interpolator)
+	{
+		interpolator.DbCommand.CommandText = interpolator.ToString();
+		return interpolator.DbCommand;
+	}
 }
