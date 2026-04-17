41c94d13e2d2e07b615ba05c0577e2ae5e57f0e0
warny <olivier.marty@gmail.com>
2026-04-17 09:29:55 +0200

    docs: refine omy.Utils package description metadata

--- UtilsTest/Expressions/FunctionCallTests.cs
+++ UtilsTest/Expressions/FunctionCallTests.cs
@@ -25,4 +25,57 @@ public class FunctionCallTests
 
         Assert.AreEqual(11, lambda());
     }
+
+    /// <summary>
+    /// Ensures delegate invocation supports array arguments.
+    /// </summary>
+    [TestMethod]
+    public void Compile_FunctionCall_WithArrayArgument_ReturnsExpectedValue()
+    {
+        var compiler = new CStyleExpressionCompiler();
+        var context = new CStyleCompilerContext();
+        context.Set("values", new[] { 1, 2, 3 });
+        context.Set("concatInt", (Func<int[], string>)(values => string.Concat(values)));
+
+        var expression = compiler.Compile("concatInt(values)", context);
+        var lambda = Expression.Lambda<Func<string>>(Expression.Convert(expression, typeof(string))).Compile();
+
+        Assert.AreEqual("123", lambda());
+    }
+
+    /// <summary>
+    /// Ensures delegate invocation supports function arguments.
+    /// </summary>
+    [TestMethod]
+    public void Compile_FunctionCall_WithLambdaArgument_ReturnsExpectedValue()
+    {
+        var compiler = new CStyleExpressionCompiler();
+        var context = new CStyleCompilerContext();
+        context.Set("apply", (Func<Func<string, string>, string, string>)((f, s) => f(s)));
+        context.Set("toUpper", (Func<string, string>)(s => s.ToUpperInvariant()));
+        context.Set("text", "aBc");
+
+        var expression = compiler.Compile("apply(toUpper, text)", context);
+        var lambda = Expression.Lambda<Func<string>>(Expression.Convert(expression, typeof(string))).Compile();
+
+        Assert.AreEqual("ABC", lambda());
+    }
+
+    /// <summary>
+    /// Ensures a method declared in source can be invoked in the same source block.
+    /// </summary>
+    [TestMethod]
+    public void Compile_FunctionDeclarationThenCall_Compiles()
+    {
+        var compiler = new CStyleExpressionCompiler();
+        var context = new CStyleCompilerContext();
+
+        var expression = compiler.CompileSource(
+            """
+            public int add(int a, int b) { a + b; }
+            add(5, 8)
+            """,
+            context);
+        Assert.IsNotNull(expression);
+    }
 }
