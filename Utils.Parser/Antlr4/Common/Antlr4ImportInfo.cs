4d394e0ad18892a6a7ff6f796fcf60b8bf475c0d
warny <olivier.marty@gmail.com>
2026-05-26 17:02:28 +0200

    Remove duplicated ANTLR prequel DTOs and old generator mapper from Utils.Parser

--- Utils.Parser/Antlr4/Common/Antlr4ImportInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace Utils.Parser.Antlr4.Common;
-
-/// <summary>
-/// Represents a single ANTLR4 <c>import</c> entry, with an optional alias.
-/// </summary>
-/// <param name="GrammarName">Imported grammar name.</param>
-/// <param name="Alias">Optional alias token used on the left side of <c>=</c> in the import declaration.</param>
-internal sealed record Antlr4ImportInfo(string GrammarName, string? Alias);
--- Utils.Parser/Antlr4/Common/Antlr4OptionSet.cs
+++
@@ -1,7 +0,0 @@
-namespace Utils.Parser.Antlr4.Common;
-
-/// <summary>
-/// Represents ANTLR4 grammar options declared in an <c>options { ... }</c> prequel block.
-/// </summary>
-/// <param name="Values">Option key/value pairs using ANTLR identifier keys.</param>
-internal sealed record Antlr4OptionSet(IReadOnlyDictionary<string, string> Values);
