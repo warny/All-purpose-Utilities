04d710a832e281a2f40cf2312795c4f4e4405899
warny <olivier.marty@gmail.com>
2026-05-26 21:43:48 +0200

    Clarify prequel diagnostic parity tests and explicit divergences

--- UtilsTest/Parser/Antlr4PrequelDiagnosticParityTests.cs
+++ UtilsTest/Parser/Antlr4PrequelDiagnosticParityTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Utils.Parser.Antlr4.Common;
@@ -12,7 +13,7 @@ namespace UtilsTest.Parser;
 public class Antlr4PrequelDiagnosticParityTests
 {
     [TestMethod]
-    public void RuntimeDiagnostics_MatchSharedPrequelFacts_ForImportsTokensChannelsAndGrammarActions()
+    public void RuntimeDiagnostics_MatchSharedPrequelFacts_ForImportsTokensAndChannels()
     {
         var runtimeBag = new DiagnosticBag();
         var definition = Antlr4GrammarConverter.ParseUnresolved(PrequelGrammar, runtimeBag);
@@ -21,20 +22,52 @@ public class Antlr4PrequelDiagnosticParityTests
         var mapped = global::Utils.Parser.Antlr4.Common.Antlr4PrequelDiagnosticMapper.ToParserDiagnostics(sharedFacts.Diagnostics);
 
         var runtimeRelevant = runtimeBag.ToList()
-            .Where(static d => d.Code is "UP1001" or "UP1002" or "UP1003" or "UP1004")
+            .Where(static d => d.Code is "UP1001" or "UP1002" or "UP1003")
             .Select(d => (d.Code, Subject: ExtractSubjectFromMessage(d.Message)))
             .ToArray();
 
         var mappedRelevant = mapped
+            .Where(static d => d.Code is "UP1001" or "UP1002" or "UP1003")
             .Select(d => (d.Code, Subject: ExtractSubjectFromMessage(d.Message)))
             .ToArray();
 
-        var mappedRuntimeRelevant = mappedRelevant.Where(static d => d.Code != "UP1004").ToArray();
-        CollectionAssert.AreEquivalent(runtimeRelevant, mappedRuntimeRelevant);
+        CollectionAssert.AreEquivalent(runtimeRelevant, mappedRelevant);
     }
 
     [TestMethod]
-    public void GeneratorDiagnostics_MatchSharedPrequelFacts_ForImportsTokensChannelsAndGrammarActions()
+    public void RuntimeDiagnostics_Divergence_GrammarActionsAreNotEmittedAsUp1004()
+    {
+        var runtimeBag = new DiagnosticBag();
+        var definition = Antlr4GrammarConverter.ParseUnresolved(PrequelGrammar, runtimeBag);
+        var prequel = Antlr4RuntimePrequelMapper.Map(definition);
+        var sharedFacts = Antlr4PrequelValidator.Validate(prequel);
+        var mapped = global::Utils.Parser.Antlr4.Common.Antlr4PrequelDiagnosticMapper.ToParserDiagnostics(sharedFacts.Diagnostics);
+
+        var runtimeActionCount = runtimeBag.ToList().Count(static d => d.Code == "UP1004");
+        var mappedActionCount = mapped.Count(static d => d.Code == "UP1004");
+
+        Assert.AreEqual(0, runtimeActionCount);
+        Assert.AreEqual(4, mappedActionCount);
+    }
+
+    [TestMethod]
+    public void GeneratorDiagnostics_Divergence_ImportDiagnosticGranularityDiffers()
+    {
+        var generatorBag = new DiagnosticBag();
+        var g4 = new G4Parser(new G4Tokenizer(PrequelGrammar).Tokenize(), generatorBag).Parse();
+        var prequel = Antlr4GeneratorPrequelMapper.Map(g4);
+        var sharedFacts = Antlr4PrequelValidator.Validate(prequel);
+        var mapped = global::Utils.Parser.Generators.Internal.Antlr4PrequelDiagnosticMapper.ToParserDiagnostics(sharedFacts.Diagnostics);
+
+        var generatorImportCount = generatorBag.ToList().Count(static d => d.Code == "UP1001");
+        var mappedImportCount = mapped.Count(static d => d.Code == "UP1001");
+
+        Assert.AreEqual(1, generatorImportCount);
+        Assert.AreEqual(2, mappedImportCount);
+    }
+
+    [TestMethod]
+    public void GeneratorDiagnostics_MatchSharedPrequelFacts_ForTokensChannelsAndGrammarActions()
     {
         var generatorBag = new DiagnosticBag();
         var g4 = new G4Parser(new G4Tokenizer(PrequelGrammar).Tokenize(), generatorBag).Parse();
@@ -43,24 +76,34 @@ public class Antlr4PrequelDiagnosticParityTests
         var mapped = global::Utils.Parser.Generators.Internal.Antlr4PrequelDiagnosticMapper.ToParserDiagnostics(sharedFacts.Diagnostics);
 
         var generatorRelevantCodes = generatorBag.ToList()
-            .Where(static d => d.Code is "UP1001" or "UP1002" or "UP1003" or "UP1004")
+            .Where(static d => d.Code is "UP1002" or "UP1003" or "UP1004")
             .GroupBy(static d => d.Code)
             .ToDictionary(static g => g.Key, static g => g.Count());
 
         var mappedRelevantCodes = mapped
-            .Where(static d => d.Code is "UP1001" or "UP1002" or "UP1003" or "UP1004")
+            .Where(static d => d.Code is "UP1002" or "UP1003" or "UP1004")
             .GroupBy(static d => d.Code)
             .ToDictionary(static g => g.Key, static g => g.Count());
 
-        Assert.AreEqual(1, generatorRelevantCodes["UP1002"]);
-        Assert.AreEqual(1, generatorRelevantCodes["UP1003"]);
-        Assert.AreEqual(4, generatorRelevantCodes["UP1004"]);
-        Assert.AreEqual(1, generatorRelevantCodes["UP1001"]);
+        AssertCodeCount(generatorRelevantCodes, "UP1002", 1);
+        AssertCodeCount(generatorRelevantCodes, "UP1003", 1);
+        AssertCodeCount(generatorRelevantCodes, "UP1004", 4);
 
-        Assert.AreEqual(1, mappedRelevantCodes["UP1002"]);
-        Assert.AreEqual(1, mappedRelevantCodes["UP1003"]);
-        Assert.AreEqual(4, mappedRelevantCodes["UP1004"]);
-        Assert.IsTrue(mappedRelevantCodes["UP1001"] >= 1);
+        AssertCodeCount(mappedRelevantCodes, "UP1002", 1);
+        AssertCodeCount(mappedRelevantCodes, "UP1003", 1);
+        AssertCodeCount(mappedRelevantCodes, "UP1004", 4);
+    }
+
+    /// <summary>
+    /// Asserts that a grouped diagnostic map contains an expected count for one code.
+    /// </summary>
+    /// <param name="counts">Diagnostic code counts.</param>
+    /// <param name="code">Diagnostic code to check.</param>
+    /// <param name="expected">Expected occurrences.</param>
+    private static void AssertCodeCount(IReadOnlyDictionary<string, int> counts, string code, int expected)
+    {
+        Assert.IsTrue(counts.TryGetValue(code, out var actual), $"Missing diagnostic code '{code}'.");
+        Assert.AreEqual(expected, actual, $"Unexpected count for diagnostic code '{code}'.");
     }
 
     private static string ExtractSubjectFromMessage(string message)
